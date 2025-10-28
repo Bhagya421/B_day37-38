1b3afd8b10193abf467d303448f4b6ba826c3470
Bhagya421 <sbhagya421@gmail.com>
2025-10-28 19:32:42 +0530

    Day 35 practice

---
+++ objectmethods.js
@@ -0,0 +1,24 @@
+//day 35
+// let emp = {
+//     name : "Bhagya",
+//     age :22,
+//     role : function(){
+//         return "mentr"
+//     }
+// };
+// console.log(emp.age);
+// console.log(emp.role());
+
+// let arr = ["nive", 22, function(){
+//     return "Hi"
+// },23,"Hello"];
+// console.log(arr[0]);
+// console.log(arr[2]());
+
+// let emp = {
+//     name : "Bhagya",
+//     age :22,
+// };
+// console.log(Object.values(emp));
+// console.log(Object.entries(emp));
+// console.log(Object.keys(emp));
